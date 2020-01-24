b7e50283f6f5dc6ff3501d3f4545388586b9796b
vamshi reddy komatireddy <vkreddy1994@gmail.com>
2020-01-24 01:20:58 -0500

    Initial commit

---
+++ taxpass/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+def home(request):
+    return render(request, template_name = 'home.html')
