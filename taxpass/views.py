ae830d49234f070589f0d361b0273595dab2c9dc
vamshi reddy komatireddy <vkreddy1994@gmail.com>
2020-02-27 20:13:29 -0500

    Fixed moth issue in signup

--- taxpass/views.py
+++ taxpass/views.py
@@ -3,6 +3,7 @@ from .forms import SingupForm, EmailForm
 from taxpass import settings
 from .models import Signup
 from django.core.mail import send_mail
+from datetime import datetime
 from django.template.loader import render_to_string
 
 
@@ -39,7 +40,10 @@ def signup(request):
             name = form.cleaned_data['name']
             phone = form.cleaned_data['phone']
             utc_callback = form.cleaned_data['utc_callback']
-            print(utc_callback.strftime("%m-%d-%Y %H:%M %Z%z"))
+            actual_time = request.POST['actual_time']
+            # print(utc_callback.strftime("%m-%d-%Y %H:%M %Z%z"))
+            client_time = datetime.strptime(actual_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+            # print(client_time)
             # obj, created = Signup.objects.update_or_create(
             #     email=email,
             #     name=name,
@@ -49,12 +53,12 @@ def signup(request):
             obj, created = None, True
             if created:
                 update_info = False
-                html_message = render_to_string('signup_email.html', {'name': name, 'time': utc_callback.strftime("%m-%d-%Y %H:%M %Z%z")})
+                html_message = render_to_string('signup_email.html', {'name': name, 'time': client_time.strftime("%m-%d-%Y %H:%M %Z%z")})
                 send_mail('TaxPass Callback Confirmation',
                           'Hi {}! We have scheduled a callback in/on: {}'.format(name, utc_callback),
                           settings.EMAIL_HOST, [form.cleaned_data['email']], html_message=html_message)
                 send_mail('Taxpass Signup - {}'.format(name), 'email: {}, phone: {}, company: {}, \
-                time: {}'.format(email, phone, name, utc_callback), settings.EMAIL_HOST,
+                time: {}'.format(email, phone, name, utc_callback.strftime("%m-%d-%Y %H:%M %Z%z")), settings.EMAIL_HOST,
                           [settings.EMAIL_HOST])
             else:
                 update_info = True
@@ -63,6 +67,10 @@ def signup(request):
             redirect_context = {'email': form.cleaned_data['email'], 'update_info': update_info, 'time': time}
             redirect_template = 'thankyou.html'
             return render(request, redirect_template, redirect_context)
+        else:
+            print("Form is invalid")
+            form = SingupForm()
+
     else:
         form = SingupForm()
     context = {'signup_form': form, 'front_email': front_email, 'form': form}
