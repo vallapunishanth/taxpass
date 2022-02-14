57b61e732d8826bc655ae0fe3a1bda7fa5ae83fe
Shreyalytical <79441201+Shreyalytical@users.noreply.github.com>
2022-02-14 19:03:40 +0530

    edit

--- taxpass/settings.py
+++ taxpass/settings.py
@@ -134,13 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/root/static/'
 STATIC_ROOT = (
     os.path.join(BASE_DIR, 'static')
 )
 
 
-MEDIA_URL = '/assets/'
+MEDIA_URL = '/root/assets/'
 MEDIA_ROOT = (
     os.path.join(BASE_DIR, 'assets')
 )
