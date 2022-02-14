6672b73b775c34860f9cec3351d037d37c264255
Shreyalytical <79441201+Shreyalytical@users.noreply.github.com>
2022-02-14 16:29:56 +0530

    edit

--- taxpass/settings.py
+++ taxpass/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'taxpass.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-if DEBUG
+if DEBUG:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-else 
+else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
